35034359aa888dbd182eb0e328ec830d0dc93aa3
Мария <mariya_piterskaya@ymail.com>
2022-10-05 09:35:57 +0300

    Задание 1

--- src/hw/Main.java
+++ src/hw/Main.java
@@ -2,6 +2,14 @@ package hw;
 
 public class Main {
     public static void main(String[] args) {
-
+        checkYear(2021);
     }
-}
+
+    private static void checkYear(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+                System.out.println(year + " — високосный год");
+            } else {
+                System.out.println(year + " — не високосный год");
+            }
+        }
+    }
